40eb3adbfc8c212b2c7449a7a2116b0e3659c17f
SprinterGameDev <sprinter@ozersk.com>
2017-05-21 20:02:15 +0500

    code style fix

--- app/src/main/java/com/architecture/standard/ui/mvp/controllers/activity/MainActivity.java
+++ app/src/main/java/com/architecture/standard/ui/mvp/controllers/activity/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends BaseActivity implements MainView, MainRouter {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         final MainPresenter presenter = new MainPresenter(this, this);
         presenter.openNextScreen();
     }
